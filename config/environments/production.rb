ecee3e4a25be6e4d166bf2863dbaafed69121fe1
Govind Upadhyay <govindupadhyay@startbitsolutions.com>
2021-04-07 11:13:18 +0530

    setup email functionality for sign-up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,10 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {:host => "https://govind-photo-app.herokuapp.com", :protocol => 'https'}
+
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
