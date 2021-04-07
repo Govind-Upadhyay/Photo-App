bc95dfce0cfb39b88984384cf9e5ff5fd5b92d5b
Govind Upadhyay <govindupadhyay@startbitsolutions.com>
2021-04-07 18:50:34 +0530

    change stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  publishable_key:  ENV['PUBLISHABLE_KEY'] || Rails.application.credentials.stripe[:stripe_publishable_key],
-  secret_key:  ENV['STRIPE_TEST_SECRET_KEY'] || Rails.application.credentials.stripe[:stripe_secret_key]
+  publishable_key:  (ENV['PUBLISHABLE_KEY'] || Rails.application.credentials.stripe[:stripe_publishable_key]),
+  secret_key:  (ENV['STRIPE_TEST_SECRET_KEY'] || Rails.application.credentials.stripe[:stripe_secret_key])
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
